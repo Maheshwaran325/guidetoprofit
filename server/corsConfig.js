32d1531868b51b087af7d1b9edeff6ccd189afc8
Maheshwaran <maheshwaran325@gmail.com>
2024-08-30 18:49:39 +0530

    cors added test 4

--- server/corsConfig.js
+++ server/corsConfig.js
@@ -1,14 +1,39 @@
-import cors from 'cors';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const corsOptions = {
+//   origin: [
+//     process.env.FRONTEND_URL,
+//     'https://cashcompassclient-maheshwaran325s-projects.vercel.app',
+//     'http://localhost:3000'
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// };
+
+// export default corsOptions;
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const corsOptions = {
-  origin: [
-    process.env.FRONTEND_URL,
-    'https://cashcompassclient-maheshwaran325s-projects.vercel.app',
-    'http://localhost:3000'
-  ],
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      process.env.FRONTEND_URL,
+      'https://cashcompassclient-maheshwaran325s-projects.vercel.app',
+      'http://localhost:3000'
+    ];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
--- server/server.js
+++ server/server.js
@@ -6,6 +6,17 @@ dotenv.config();
 
 const port = process.env.PORT || 8000;
 
+// Add a root route
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 app.listen(port, () => {
   logger.info(`Server is running on port ${port}`);
 });
